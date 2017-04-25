fb852b54965bf6ea20223ec7e5790ac14d4f0135
XadillaX <admin@xcoder.in>
2017-04-25 14:17:01 +0800

    feat: cpp as entry

---
+++ 2. cpp entry/binding.gyp
@@ -0,0 +1,8 @@
+{
+  "targets": [{
+    "target_name": "entry",
+    "sources": [
+      "entry.cpp"
+    ]
+  }]
+}
---
+++ 2. cpp entry/entry.cpp
@@ -0,0 +1,41 @@
+#include <node.h>
+
+namespace __entry__ {
+
+using v8::FunctionCallbackInfo;
+using v8::Function;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Value;
+using v8::HandleScope;
+
+void RunCallback(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope(isolate);
+
+    Local<Function> cb = Local<Function>::Cast(args[0]);
+    const unsigned argc = 1;
+    Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "hello world") };
+    cb->Call(isolate->GetCurrentContext()->Global(), argc, argv);
+}
+
+void init(Local<Object> exports, Local<Object> module)
+{
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope(isolate);
+
+    NODE_SET_METHOD(exports, "runCallback", RunCallback);
+
+    Local<Object> global = isolate->GetCurrentContext()->Global();
+    Local<Object> console = global->Get(String::NewFromUtf8(isolate, "console"))->ToObject();
+    Local<Function> log = Local<Function>::Cast(console->Get(String::NewFromUtf8(isolate, "log")));
+    Local<Value> argv[3] = { module, String::NewFromUtf8(isolate, "---"), exports };
+
+    log->Call(console, 3, argv);
+}
+
+NODE_MODULE(addon, init)
+
+}
