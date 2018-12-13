31e46955664387dcad12f61fb4e895f0678e95c7
XadillaX <i@2333.moe>
2018-12-13 18:26:36 +0800

    fix: make code 5 compilable under Node.js 11.x (#2)

--- 5. object template accessor/accessor.cpp
+++ 5. object template accessor/accessor.cpp
@@ -2,6 +2,7 @@
 
 namespace __accessor__ {
 
+using v8::Context;
 using v8::External;
 using v8::FunctionCallbackInfo;
 using v8::HandleScope;
@@ -21,6 +22,10 @@ using v8::PropertyCallbackInfo;
 
 int x = 0;
 
+// Mentioned @ https://github.com/XadillaX/nyaa-nodejs-demo/issues/2
+#define CURRENT_CONTEXT(ctx) Isolate* isolate = Isolate::GetCurrent(); \
+                                Local<Context> ctx = isolate->GetCurrentContext();
+
 void Getter1(Local<String> property, const PropertyCallbackInfo<Value>& info)
 {
     info.GetReturnValue().Set(x);
@@ -28,17 +33,33 @@ void Getter1(Local<String> property, const PropertyCallbackInfo<Value>& info)
 
 void Setter1(Local<String> property, Local<Value> value, const PropertyCallbackInfo<void>& info)
 {
+#if NODE_MODULE_VERSION >= 67 // Node.js 11.0.0+
+    CURRENT_CONTEXT(context);
+    x = value->Int32Value(context).FromJust();
+#else
     x = value->Int32Value();
+#endif
 }
 
 void Getter2(Local<String> property, const PropertyCallbackInfo<Value>& info)
 {
+#if NODE_MODULE_VERSION >= 67 // Node.js 11.0.0+
+    CURRENT_CONTEXT(context);
+    info.GetReturnValue().Set(
+            info.Data()->ToObject(context).ToLocalChecked()->Get(String::NewFromUtf8(info.GetIsolate(), "inner")));
+#else
     info.GetReturnValue().Set(info.Data()->ToObject()->Get(String::NewFromUtf8(info.GetIsolate(), "inner")));
+#endif
 }
 
 void Setter2(Local<String> property, Local<Value> value, const PropertyCallbackInfo<void>& info)
 {
+#if NODE_MODULE_VERSION >= 67
+    CURRENT_CONTEXT(context);
+    info.Data()->ToObject(context).ToLocalChecked()->Set(String::NewFromUtf8(info.GetIsolate(), "inner"), value);
+#else
     info.Data()->ToObject()->Set(String::NewFromUtf8(info.GetIsolate(), "inner"), value);
+#endif
 }
 
 class TestExternal {
@@ -90,12 +111,20 @@ void Getter3(Local<String> property, const PropertyCallbackInfo<Value>& info)
 
 void Setter3(Local<String> property, Local<Value> value, const PropertyCallbackInfo<void>& info)
 {
+#if NODE_MODULE_VERSION >= 67 // Node.js 11.0.0+
+    CURRENT_CONTEXT(context);
+#endif
+
     Local<Object> self = info.Holder();
     Local<External> wrap = Local<External>::Cast(self->GetInternalField(0));
     void* ptr = wrap->Value();
     TestExternal* ex = (TestExternal*)ptr;
 
+#if NODE_MODULE_VERSION >= 67 // Node.js 11.0.0+
+    ex->Set(value->ToInt32(context).ToLocalChecked()->Value());
+#else
     ex->Set(value->ToInt32()->Value());
+#endif
 }
 
 void Init(Local<Object> exports, Local<Object> module)
